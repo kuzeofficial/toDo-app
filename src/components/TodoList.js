fc673676a53e6eaf8c295e08fc048449629335f8
Cristian Fonseca <cristiancomas@hotmail.com>
2021-06-17 23:47:35 -0700

    Todo Form created

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+
+const TodoList = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TodoList
